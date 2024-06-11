d917ff29087bc8a0bfa0593c450d3d7c7bb74183
Júlia Martins Souza <juliamss07@gmail.com>
2024-06-11 20:17:47 -0300

    Update ContaCorrente.java

--- src./src/ContaCorrente.java
+++ src./src/ContaCorrente.java
@@ -1,13 +1,16 @@
 public class ContaCorrente extends  Conta {
-    public ContaCorrente(Cliente donoConta, double saldo, int num) {
+    public ContaCorrente(Cliente donoConta, double limite, double saldo, int num) {
         super(donoConta, saldo, num);
+        setLimite(limite);
     }
 
     @Override
     public boolean setLimite(double limite) {
-        if (limite < -100) {
+        if (limite <= -100) {
+            super.limite = -100;
             return false;
         } else {
+            super.limite = limite;
             return true;
         }
     }
